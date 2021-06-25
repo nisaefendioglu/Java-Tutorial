bafcdd440779116875ac3338eb3031cf3065a495
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-06-25 21:45:00 +0300

    üçüncü hafta ders kodları.

---
+++ Hafta 3/gelismisForDongusu/src/gelismisfordongusu/GelismisForDongusu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gelismisfordongusu;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class GelismisForDongusu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] dizi={1,2,3,4,5};
+        for(int i: dizi){
+            System.out.println(i+""); 
+        }
+    }
+    
+}
