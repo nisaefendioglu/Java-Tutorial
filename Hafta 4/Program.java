0d42b4774f8c698d6e00454ca81ec63b0ace33a5
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-07-02 21:44:23 +0300

    dördüncü hafta ders kodları.

---
+++ Hafta 4/Program.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Program {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] sayi = {1,5,8,9};
+        System.out.println(Arrays.toString(sayi));
+
+
+        
+   }
+}
